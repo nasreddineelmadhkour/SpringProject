70f5605e48cc9e00449790168489987e1bfce809
Nasreddine El Madhkour <90349923+nasreddineelmadhkour@users.noreply.github.com>
2022-10-13 12:08:46 +0100

    relation entre les entites

--- src/main/java/tn/esprit/sprintproject/SpringProjectApplication.java
+++ src/main/java/tn/esprit/sprintproject/SpringProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SprintProjectApplication {
+public class SpringProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SprintProjectApplication.class, args);
+		SpringApplication.run(SpringProjectApplication.class, args);
 	}
 
 }
